78df4a9570cacbbcb4262fcfeed892aa5be5fe13
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-02-11 22:40:20 +0800

    Add QuickCheck

--- tests/test.hs
+++ tests/test.hs
@@ -1,5 +1,6 @@
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck as QC
 
 import Data.List
 import Data.Ord
@@ -7,7 +8,21 @@ import Data.Ord
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Tests" [properties, unitTests]
+
+properties :: TestTree
+properties = testGroup "Properties" [props]
+
+props = testGroup "(checked by QuickCheck)"
+  [ QC.testProperty "sort == sort . reverse" $
+      \list -> sort (list :: [Int]) == sort (reverse list)
+  , QC.testProperty "Fermat's little theorem" $
+      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
+  -- the following property does not hold
+  , QC.testProperty "Fermat's last theorem" $
+      \x y z n ->
+        (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
+  ]
 
 unitTests = testGroup "Unit tests"
   [ testCase "List comparison (different length)" $
